a33d6044f2e056b83e1f3b104b1f6149f828d8c7
kimsan1120 <ks20001120@gmail.com>
2024-01-21 20:22:18 +0900

    buttons, router main
    deleted buttons and handled router to main if success

--- side3/components/contents/content-picker.js
+++ side3/components/contents/content-picker.js
@@ -1,5 +1,5 @@
-'use client';
-import { useRef, useState } from 'react';
+"use client";
+import { useRef, useState } from "react";
 
 export default function ContentPicker({ label, name }) {
   const [pickedContent, setPickedContent] = useState();
@@ -20,7 +20,7 @@ export default function ContentPicker({ label, name }) {
     fileReader.onload = () => {
       setPickedContent({
         data: fileReader.result,
-        type: file.type.split('/')[0], // 'image' or 'video'
+        type: file.type.split("/")[0], // 'image' or 'video'
       });
     };
     fileReader.readAsDataURL(file);
@@ -28,16 +28,14 @@ export default function ContentPicker({ label, name }) {
 
   return (
     <div className="flex flex-col">
-      <label htmlFor={name} className="font-medium text-lg">{label}</label>
+      <label htmlFor={name} className="font-medium text-lg">
+        {label}
+      </label>
       <div className="flex items-start gap-6 mb-4">
-        <div className="w-40 h-40 border-2 border-gray-400 flex justify-center items-center text-center text-gray-400 relative">
+        <div onClick={handlePickClick} className="w-40 h-40 border-2 border-blue-400 flex justify-center items-center text-center text-gray-400 relative">
           {!pickedContent && <p className="m-0 p-4">No content picked yet.</p>}
-          {pickedContent && pickedContent.type === 'image' && (
-            <img src={pickedContent.data} alt="The content selected by the user." className="max-w-full max-h-full object-cover" />
-          )}
-          {pickedContent && pickedContent.type === 'video' && (
-            <video src={pickedContent.data} alt="The content selected by the user." className="max-w-full max-h-full" controls />
-          )}
+          {pickedContent && pickedContent.type === "image" && <img src={pickedContent.data} alt="The content selected by the user." className="max-w-full max-h-full object-cover" />}
+          {pickedContent && pickedContent.type === "video" && <video src={pickedContent.data} alt="The content selected by the user." className="max-w-full max-h-full" controls />}
         </div>
         <input
           type="file"
@@ -49,13 +47,6 @@ export default function ContentPicker({ label, name }) {
           // required
           className="hidden"
         />
-        <button
-          type="button"
-          onClick={handlePickClick}
-          className="border-0 py-2 px-6 bg-gray-400 rounded cursor-pointer hover:bg-gray-500"
-        >
-          Pick an Image/Video
-        </button>
       </div>
     </div>
   );
